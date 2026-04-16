be507c1b42d18c8cfd3066385c80364f6e44fd36
Thomas Krajacic <thomas@krajacic.at>
2026-04-16 21:53:02 +0200

    Add WeatherLibrary type that clashes with module name

---
+++ Shared/weather-lib/Sources/WeatherLibrary/WeatherLibrary.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherLibrary.swift
+//  WeatherLibrary
+//
+//  Created by Thomas Krajacic on 16.04.26.
+//
+
+public final class WeatherLibrary {
+    public init() {
+        //
+    }
+    
+    public func test() {
+        
+    }
+}
+
+public final class WeatherLibrary1 {
+    public init() {
+        //
+    }
+    
+    public func test() {
+        
+    }
+}
